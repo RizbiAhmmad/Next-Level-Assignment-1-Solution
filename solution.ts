3c3200437a97957a14a6fbb20a688debae3d21a3
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-11-18 01:11:00 +0600

    Problem 4 Solved

--- solution.ts
+++ solution.ts
@@ -1,45 +1,63 @@
-function formatValue(value:string | number | boolean):string | number | boolean{
-    if (typeof value==="string"){
-        return value.toUpperCase()
-    }
-    if (typeof value==="number"){
-        return value*10
-    }
-    if (typeof value==="boolean"){
-        return !value
-    }
-    return value;
+function formatValue(
+  value: string | number | boolean
+): string | number | boolean {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  }
+  if (typeof value === "number") {
+    return value * 10;
+  }
+  if (typeof value === "boolean") {
+    return !value;
+  }
+  return value;
 }
 // console.log(formatValue('hello'));
 // console.log(formatValue(5));
 // console.log(formatValue(true));
 
-function getLength( value:string | any[]):number{
-    if (typeof value ==="string"){
-        return value.length
-    }
-    if (Array.isArray(value)){
-        return value.length
-    }
-    return 0;
+function getLength(value: string | any[]): number {
+  if (typeof value === "string") {
+    return value.length;
+  }
+  if (Array.isArray(value)) {
+    return value.length;
+  }
+  return 0;
 }
 // console.log(getLength('types'));
 // console.log(getLength([10, 20, 30, 40, 50, 60]));
 
-class Person{
-    name:string;
-    age:number;
+class Person {
+  name: string;
+  age: number;
 
-    constructor(name:string , age: number) {
-    this.name =name
-    this.age = age
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
 }
-getDetails():string {
-   return `'Name: ${this.name}, Age: ${this.age}'`
-}
-}
-const person1 = new Person('John Doe', 30);
+const person1 = new Person("John Doe", 30);
 // console.log(person1.getDetails());
 
-const person2 = new Person('Alice', 25);
+const person2 = new Person("Alice", 25);
 // console.log(person2.getDetails());
+
+type Item = {
+  title: string;
+  rating: number;
+};
+function filterByRating(items: Item[]): Item[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+const books: Item[] = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+// console.log(filterByRating(books));
