b175dd30fa7e086de38d9eaba46c0486a41dabd3
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-11-18 00:23:28 +0600

    Problem 3 Solved

--- solution.ts
+++ solution.ts
@@ -25,3 +25,21 @@ function getLength( value:string | any[]):number{
 }
 // console.log(getLength('types'));
 // console.log(getLength([10, 20, 30, 40, 50, 60]));
+
+class Person{
+    name:string;
+    age:number;
+
+    constructor(name:string , age: number) {
+    this.name =name
+    this.age = age
+}
+getDetails():string {
+   return `'Name: ${this.name}, Age: ${this.age}'`
+}
+}
+const person1 = new Person('John Doe', 30);
+// console.log(person1.getDetails());
+
+const person2 = new Person('Alice', 25);
+// console.log(person2.getDetails());
